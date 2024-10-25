7fb35e6b98011d16873b0377bc5f40e75400e5de
culhaci22 <erenculhaci@hotmail.com>
2024-10-25 14:31:28 +0300

    Updated services
    Excluded analytics, location, promotion services added auth and ranking services

---
+++ BiteScout-Microservices/Ranking-Service/src/test/java/com/bitescout/app/rankingservice/RankingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bitescout.app.rankingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RankingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
