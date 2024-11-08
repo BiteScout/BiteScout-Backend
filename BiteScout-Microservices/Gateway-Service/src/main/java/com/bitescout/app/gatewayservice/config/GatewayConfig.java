99d29025e0866ede52525ccc1d7974317ea9d78d
furkan <frkn9@hotmail.com>
2024-11-08 10:16:01 +0300

    reservations

---
+++ BiteScout-Microservices/Gateway-Service/src/main/java/com/bitescout/app/gatewayservice/config/GatewayConfig.java
@@ -0,0 +1,47 @@
+package com.bitescout.app.gatewayservice.config;
+
+import com.bitescout.app.gatewayservice.filter.JwtAuthenticationFilter;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+
+    private final JwtAuthenticationFilter filter;
+
+    public GatewayConfig(JwtAuthenticationFilter filter) {
+        this.filter = filter;
+    }
+
+    @Bean
+    public RouteLocator routes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("authentication-service", r -> r.path("/api/v1/auth/**")
+                        .uri("lb://AUTHENTICATION-SERVICE"))
+
+                .route("notification-service", r -> r.path("/api/v1/notifications/**")
+                        .filters(f -> f.filter(filter)) // Apply the JWT filter here
+                        .uri("lb://NOTIFICATION-SERVICE"))
+
+                .route("ranking-service", r -> r.path("/api/v1/ranking/**")
+                        .uri("lb://RANKING-SERVICE"))
+
+                .route("reservation-service", r -> r.path("/api/v1/reservations/**")
+                        .filters(f -> f.filter(filter))
+                        .uri("lb://RESERVATION-SERVICE"))
+
+                .route("restaurant-service", r -> r.path("/api/v1/restaurants/**")
+                        .uri("lb://RESTAURANT-SERVICE"))
+
+                .route("review-service", r -> r.path("/api/v1/reviews/**")
+                        .uri("lb://REVIEW-SERVICE"))
+
+                .route("user-service", r -> r.path("/api/v1/users/**")
+                        .filters(f -> f.filter(filter))
+                        .uri("lb://USER-SERVICE"))
+
+                .build();
+    }
+}
