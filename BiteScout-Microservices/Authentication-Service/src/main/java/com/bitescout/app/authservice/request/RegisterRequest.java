22afaea1fcdd3c21c7cd8092963e52f0cd54c9f5
ömer yıldız <yildizom20@itu.edu.tr>
2024-11-22 19:44:33 +0300

    user-serice app.yml and auth register request dto fixed

--- BiteScout-Microservices/Authentication-Service/src/main/java/com/bitescout/app/authservice/request/RegisterRequest.java
+++ BiteScout-Microservices/Authentication-Service/src/main/java/com/bitescout/app/authservice/request/RegisterRequest.java
@@ -3,7 +3,11 @@ package com.bitescout.app.authservice.request;
 import com.bitescout.app.authservice.enums.Role;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import jakarta.validation.constraints.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Email;
 import lombok.Data;
 import lombok.Getter;
 
