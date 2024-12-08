23c329d481411da2a1923a0456f41d55742c1075
furkan <frkn9@hotmail.com>
2024-12-08 09:44:09 +0300

    bugfix

--- BiteScout-Microservices/Notification-Service/src/main/java/com/bitescout/app/notificationservice/NotificationServiceApplication.java
+++ BiteScout-Microservices/Notification-Service/src/main/java/com/bitescout/app/notificationservice/NotificationServiceApplication.java
@@ -2,12 +2,14 @@ package com.bitescout.app.notificationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableJpaAuditing
 @SpringBootApplication
 @EnableAsync
+@EnableFeignClients
 public class NotificationServiceApplication {
 
     public static void main(String[] args) {
