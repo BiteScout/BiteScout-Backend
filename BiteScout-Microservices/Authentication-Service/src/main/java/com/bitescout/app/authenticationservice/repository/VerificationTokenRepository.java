928b458b69d2877723ff428bff9f472d2d0a237d
culhaci22 <erenculhaci@hotmail.com>
2024-12-17 14:28:47 +0300

    fix repo for auth

--- BiteScout-Microservices/Authentication-Service/src/main/java/com/bitescout/app/authenticationservice/repository/VerificationTokenRepository.java
+++ BiteScout-Microservices/Authentication-Service/src/main/java/com/bitescout/app/authenticationservice/repository/VerificationTokenRepository.java
@@ -2,9 +2,11 @@ package com.bitescout.app.authenticationservice.repository;
 
 import com.bitescout.app.authenticationservice.entity.VerificationToken;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface VerificationTokenRepository extends JpaRepository<VerificationToken, Long> {
     Optional<VerificationToken> findByToken(String token);
 }
