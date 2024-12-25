6f169a43772e31a9517bca91cb6f046551cbef78
ömer yıldız <yildizom20@itu.edu.tr>
2024-12-25 12:06:07 +0300

    notif

---
+++ BiteScout-Microservices/Notification-Service/src/main/java/com/bitescout/app/notificationservice/security/SecurityService.java
@@ -0,0 +1,17 @@
+package com.bitescout.app.notificationservice.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class SecurityService {
+
+
+
+
+
+
+
+
+}
