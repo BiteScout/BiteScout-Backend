14214cd1b7b5f8990c75ee071be7c6d5d367f5d7
furkan <frkn9@hotmail.com>
2025-01-01 14:20:53 +0300

    added validators to review interaction

--- BiteScout-Microservices/Review-Service/src/main/java/com/bitescout/app/reviewservice/review/dto/ReviewInteractionRequest.java
+++ BiteScout-Microservices/Review-Service/src/main/java/com/bitescout/app/reviewservice/review/dto/ReviewInteractionRequest.java
@@ -1,10 +1,19 @@
 package com.bitescout.app.reviewservice.review.dto;
 
 import com.bitescout.app.reviewservice.review.InteractionType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 public record ReviewInteractionRequest(
+        @NotBlank
         String reviewId,
+
+        @NotBlank
         InteractionType interactionType,
+
+        @Size(min = 8, message = "Comment must be at least 8 characters long")
+        @Size(max = 1024, message = "Comment must be at must 1024 characters long ")
         String replyText
 ) {
 }
