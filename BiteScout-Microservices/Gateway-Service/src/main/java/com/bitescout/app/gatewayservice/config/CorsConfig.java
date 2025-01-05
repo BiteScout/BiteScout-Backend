71a9d4436bb96a9594e2c37146b6aab41fbd344a
culhaci22 <erenculhaci@hotmail.com>
2025-01-05 12:47:04 +0300

    fix cors config

--- BiteScout-Microservices/Gateway-Service/src/main/java/com/bitescout/app/gatewayservice/config/CorsConfig.java
+++ BiteScout-Microservices/Gateway-Service/src/main/java/com/bitescout/app/gatewayservice/config/CorsConfig.java
@@ -3,23 +3,23 @@ package com.bitescout.app.gatewayservice.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsWebFilter;
 
 @Configuration
 public class CorsConfig {
 
     @Bean
-    public CorsFilter corsFilter() {
+    public CorsWebFilter corsWebFilter() {
         CorsConfiguration config = new CorsConfiguration();
         config.addAllowedOriginPattern("https://bitescout.netlify.app"); // Allow your Netlify domain
-        config.addAllowedMethod("*");  // Allow all HTTP methods (GET, POST, etc.)
+        config.addAllowedMethod("*");  // Allow all HTTP methods
         config.addAllowedHeader("*");  // Allow all headers
         config.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
 
-        return new CorsFilter(source);
+        return new CorsWebFilter(source);
     }
 }
