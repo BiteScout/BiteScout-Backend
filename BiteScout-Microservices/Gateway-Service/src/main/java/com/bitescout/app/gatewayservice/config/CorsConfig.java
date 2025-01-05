c0fa35781cd0c8b286bfea7a0bc7412043c95902
culhaci22 <erenculhaci@hotmail.com>
2025-01-05 12:23:39 +0300

    cors config

---
+++ BiteScout-Microservices/Gateway-Service/src/main/java/com/bitescout/app/gatewayservice/config/CorsConfig.java
@@ -0,0 +1,25 @@
+package com.bitescout.app.gatewayservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOriginPattern("https://bitescout.netlify.app"); // Allow your Netlify domain
+        config.addAllowedMethod("*");  // Allow all HTTP methods (GET, POST, etc.)
+        config.addAllowedHeader("*");  // Allow all headers
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsFilter(source);
+    }
+}
